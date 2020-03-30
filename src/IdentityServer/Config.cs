e7b4a9802a4727ea97b71d222f2a7fa9591b1fe7
RioShi <rio.shi@grapecity.com>
2020-03-30 18:13:37 +0900

    add mvc and test ok in ff

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -11,25 +12,43 @@ namespace IdentityServer
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { 
+            new ApiResource[]
+            {
                 new ApiResource("api1", "My API")
             };
-        
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
+            new Client[]
             { 
+                // interactive ASP.NET Core MVC client
                 new Client
                 {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:8002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:8002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
-        
+
     }
 }
